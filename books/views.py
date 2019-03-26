e2775e0718e9a6a75e382f907ec4c4251cff9e79
TheMiloAnderson <themiloanderson@gmail.com>
2019-03-26 15:30:38 -0700

    added book app, model, templates, views

--- books/views.py
+++ books/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404, get_object_or_404
+from .models import Book
 
-# Create your views here.
+
+def book_detail_view(req):
+    context = {
+        book: get_object_or_404(Book, id=pk)
+    }
+    return render(req, 'book_detail.html', context)
+
+
+def book_list_view(req):
+    context = {
+        books: get_list_or_404(Book)
+    }
+    return render(req, 'books_list.html', context)
