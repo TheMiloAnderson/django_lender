ef8c66f1918ad66d0009b6d44c63d9d345d08a64
TheMiloAnderson <themiloanderson@gmail.com>
2019-03-28 17:42:29 -0700

    added book image from google API

--- books/views.py
+++ books/views.py
@@ -1,16 +1,22 @@
 from django.shortcuts import render, get_list_or_404, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from .models import Book
+import requests
 
 
 @login_required
 def book_detail_view(req, pk=None):
+    book = get_object_or_404(
+        Book,
+        id=pk,
+        user=req.user.id
+    )
+    response = requests.get(
+        f'https://www.googleapis.com/books/v1/volumes?q=+intitle:{ book.title }')
+    data = response.json()
     context = {
-        'book': get_object_or_404(
-            Book,
-            id=pk,
-            user=req.user.id
-        )
+        'book': book,
+        'image_url': data['items'][0]['volumeInfo']['imageLinks']['thumbnail']
     }
     return render(req, 'book_detail.html', context)
 
