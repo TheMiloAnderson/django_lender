26a29617d410170f0dd62d1589eb43cfbbf6e951
TheMiloAnderson <themiloanderson@gmail.com>
2019-03-31 19:40:08 -0700

    built books list with Vue component

--- books/views.py
+++ books/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render, get_list_or_404, get_object_or_404
+from django.urls import reverse
+from django.core import serializers
+from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from .models import Book
 import requests
+import json
 
 
 @login_required
@@ -12,7 +16,7 @@ def book_detail_view(req, pk=None):
         user=req.user.id
     )
     response = requests.get(
-        f'https://www.googleapis.com/books/v1/volumes?q=+intitle:{ book.title }')
+        f'https://www.googleapis.com/books/v1/volumes?q=+intitle:{book.title}')
     data = response.json()
     context = {
         'book': book,
@@ -23,7 +27,22 @@ def book_detail_view(req, pk=None):
 
 @login_required
 def book_list_view(req):
-    context = {
-        'books': get_list_or_404(Book, user=req.user.id)
-    }
-    return render(req, 'books_list.html', context)
+    return render(req, 'books_list.1.html')
+
+
+@login_required
+def book_list_data(req):
+    books = Book.objects.filter(user=req.user.id)
+    books_list = []
+    for book in books:
+        books_list.append({
+            'title': book.title,
+            'author': book.author,
+            'status': book.status,
+            'id': book.id,
+            'url': reverse('book_detail', args=(book.id,))
+        })
+    return HttpResponse(
+        json.dumps(books_list),
+        content_type='application/json'
+    )
